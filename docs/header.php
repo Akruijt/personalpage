3816874160fab1776f961a680bc5ffa610ec2b4d
Akruijt <kruijtanne@gmail.com>
2019-10-14 14:58:30 +0200

    Test new navbar in separate file

---
+++ docs/header.php
@@ -0,0 +1,11 @@
+ <div class="topnav">
+    <a href="index.html">Home</a>
+    <a href="cv.html">CV</a>
+    <a href="publications.html">Publications</a>
+    <a href="recordings.html">Audio files</a>
+    <a class="active" href="gallery.html">Gallery</a>
+    <a  href="bingo.html">Conference Bingo</a>
+    <a href="map.html">Where in the world am I?</a>
+    <a style="float:right" href="nl/gallery_nl.html">NL <img src="icons/nederland.png" style="height:16px; width:16px;"></a>
+    <a style="float:right" href="gallery.html">EN <img src="icons/canada.png" style="height:16px; width:16px;"></a>
+ </div>
